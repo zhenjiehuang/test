d35c019b78857c7475bbe8ddbe445c6a5426dd11
huangzhenjie <Administrator@AFOGL-607281025>
2017-09-14 10:05:57 +0800

    12345

--- src/redis/Test.java
+++ src/redis/Test.java
@@ -1,15 +1,22 @@
 package redis;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisShardInfo;
 
 public class Test {
     public static void main(String[] args) {
         try {
-            Jedis jedis = new Jedis("127.0.0.1", 6379);
-
-            for (String key : jedis.keys("*")) {
-                System.out.println(key);
+            JedisShardInfo info = new JedisShardInfo("10.46.74.95", 9999);
+            info.setPassword("6dbc0460f9a34cf9:Huimei2015");
+            Jedis jedis = new Jedis(info);
+            for (byte[] key : jedis.keys("*".getBytes())) {
+                if (new String(key).contains("express")) {
+                    System.out.println(new String(key));
+                }
+                // System.out.println(new String(key));
             }
+
+            System.out.println(jedis.get("express_1"));
 
             jedis.close();
         } catch (Exception e) {
